044291e15c14fefb1fca2f0d075602ac324f394a
MZiolkowski <maciej.macron.ziolkowski@gmail.com>
2023-10-02 19:54:16 +0200

    Update key-sender.js

--- key-sender.js
+++ key-sender.js
@@ -110,9 +110,9 @@ module.exports = function() {
 
     module.execute = function(arrParams) {
         return new Promise(function(resolve, reject) {
-            var jarPath = path.join(__dirname, 'jar', 'key-sender.jar');
+            var jarPath = path.join(__dirname, '..', '..', '..', 'node_modules', 'node-key-sender', 'jar', 'key-sender.jar');
 
-            var command = 'java -jar \"' + jarPath + '\" ' + arrParams.join(' ') + module.getCommandLineOptions();
+            var command = 'java -jar "' + jarPath + '" ' + arrParams.join(' ') + module.getCommandLineOptions();
 
             return exec(command, {}, function(error, stdout, stderr) {
                 if (error == null) {
